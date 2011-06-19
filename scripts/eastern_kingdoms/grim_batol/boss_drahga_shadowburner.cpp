8e23b9e060a9eab0a0bfc1a4a8e0bf4b6f3777a3
Azerus <azerus21@mail.ru>
2011-06-19 22:28:31 +0600

    [cata007] Grim Batol - Add Filters and Boss files

--- scripts/eastern_kingdoms/grim_batol/boss_drahga_shadowburner.cpp
+++ scripts/eastern_kingdoms/grim_batol/boss_drahga_shadowburner.cpp
@@ -1,5 +1,4 @@
 /* Copyright (C) 2006 - 2011 ScriptDev2 <http://www.scriptdev2.com/>
- * Copyright (C) 2005 - 2011 MaNGOS <http://www.getmangos.com/>
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -16,14 +15,10 @@
  */
 
 /* ScriptData
-SDName: gilneas
-SD%Complete: 0
-SDComment: Quest support:
-SDCategory: Gilneas
+SDName: 
+SD%Complete:
+SDComment:
+SDCategory: 
 EndScriptData */
 
 #include "precompiled.h"
-
-void AddSC_gilneas()
-{
-}
